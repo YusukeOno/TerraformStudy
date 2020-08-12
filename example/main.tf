75e3de062d13bf7a536ab63c787b6a5846dd067d
小野裕輔 <y-ono@nswt.co.jp>
2020-08-13 08:06:41 +0900

    Add list 3.2 ローカル変数の定義

--- example/main.tf
+++ example/main.tf
@@ -2,10 +2,14 @@ variable "example_instance_type" {
   default = "t3.micro"
 }
 
+locals {
+  example_subnet_id = "subnet-0e593a21b30d6547d"
+}
+
 resource "aws_instance" "example" {
   ami           = "ami-0c3fd0f5d33134a76"
   instance_type = var.example_instance_type
-  subnet_id     = "subnet-0e593a21b30d6547d"
+  subnet_id     = local.example_subnet_id
 
   user_data = <<EOF
     #!/bin/bash
