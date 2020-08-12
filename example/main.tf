4f63790f8f6619bd469087ab28687b90c60163f8
小野裕輔 <y-ono@nswt.co.jp>
2020-08-13 01:54:53 +0900

    Add subnet_id

--- example/main.tf
+++ example/main.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "example" {
   ami           = "ami-0c3fd0f5d33134a76"
   instance_type = "t3.micro"
+  subnet_id     = "subnet-0e593a21b30d6547d"
 
   user_data = <<EOF
     #!/bin/bash
