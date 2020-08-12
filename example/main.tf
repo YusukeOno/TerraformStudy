3e1d738729b7d495c8939127fb3aebb1b5d7bf9d
小野裕輔 <y-ono@nswt.co.jp>
2020-08-13 07:58:31 +0900

    Add list 3.1 変数の定義

--- example/main.tf
+++ example/main.tf
@@ -1,6 +1,10 @@
+variable "example_instance_type" {
+  default = "t3.micro"
+}
+
 resource "aws_instance" "example" {
   ami           = "ami-0c3fd0f5d33134a76"
-  instance_type = "t3.micro"
+  instance_type = var.example_instance_type
   subnet_id     = "subnet-0e593a21b30d6547d"
 
   user_data = <<EOF
