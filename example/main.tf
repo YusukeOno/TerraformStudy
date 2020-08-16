1e443a50e58e6afd1a5121191be7588453820a77
小野裕輔 <y-ono@nswt.co.jp>
2020-08-16 22:40:09 +0900

    Add list 3.4 データソースの定義

--- example/main.tf
+++ example/main.tf
@@ -1,3 +1,22 @@
+data "aws_ami" "recent_amazon_linux_2" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-2.0.????????-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "state"
+    values = ["available"]
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
 variable "example_instance_type" {
   default = "t3.micro"
 }
