aa9e6ef63e2de39c593fb9eb52523ec68724be2c
小野裕輔 <y-ono@nswt.co.jp>
2020-08-23 22:56:16 +0900

    Add list 3.6 セキュリティグループの定義

--- example/main.tf
+++ example/main.tf
@@ -37,6 +37,24 @@ resource "aws_instance" "example" {
 EOF
 }
 
+resource "aws_security_group" "example_ec2" {
+  name = "example-ec2"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 output "example_instance_id" {
   value = aws_instance.example.id
 }
