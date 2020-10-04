1821dc63a9ea5dbbc59ebd5d0dae6c1cf74776b6
小野裕輔 <y-ono@nswt.co.jp>
2020-10-04 13:05:08 +0900

    Add list 5.4 信頼ポリシーの定義

--- practice/main.tf
+++ practice/main.tf
@@ -10,3 +10,14 @@ resource "aws_iam_policy" "example" {
   name = "example"
   policy = data.aws_iam_policy_document.allow_describe_regions.json
 }
+
+data "aws_iam_policy_document" "ec2_assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
